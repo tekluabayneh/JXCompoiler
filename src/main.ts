4384aff473439948d76398c0a4295998ab5a04f5
teklu abayneh <tekluabayneh@gmail.com>
2025-05-13 23:33:38 +0300

    adding the amp list to test if it work with also maping element

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,24 @@ console.log(
     children: ["name"],
   })
 );
+let Element = render({
+  NodeName: "h1",
+  props: { ClassName: "heading" },
+  children: ["name"],
+});
+
+let list = [
+  {
+    NodeName: "h1",
+    props: { ClassName: "heading" },
+    children: ["name"],
+  },
+  { NodeName: "p", props: { ClassName: "paragraph" }, children: ["this is the long paragrah may  be this one work man"] },
+  { NodeName: "div", props: { ClassName: "div" }, children: ["this is the div Element"] },
+  { NodeName: "section", props: { ClassName: "section" }, children: ["this is the section Element"] },
+];
+
+let maped = list.map((Element) => {
+  document.body.appendChild(render(Element));
+});
+document.body.appendChild(Element);
